832a6bd418385224da8caba7d4498e02d61f8fc1
brkuvezdic <branimirkuvezdic@gmail.com>
2024-01-23 18:30:37 +0100

    Connected Vercel to frontend-testing deployment with changes

--- client/src/pages/selectedFeaturedEvent.jsx
+++ client/src/pages/selectedFeaturedEvent.jsx
@@ -1,17 +1,15 @@
-// SelectedFeaturedEvent.jsx
 import React from "react";
 import { useParams } from "react-router-dom";
-import { getEventById } from "../store/store"; // Ažurirajte putanju prema vašem direktoriju
+import { getEventById } from "../store/store";
 function SelectedFeaturedEvent() {
   const { eventId } = useParams();
-  const event = getEventById(Number(eventId)); // Pretpostavljamo da je eventId broj
+  const event = getEventById(Number(eventId));
 
   return (
-    <div>
-      <h2>{event.title}</h2>
-      <p>{event.description}</p>
-      <p>Organizirao/la: {event.organizer}</p>
-      {/* Dodajte ostale informacije koje želite prikazati */}
+    <div className="max-w-xl mx-auto mt-10 p-6 bg-sky-300 shadow-md rounded-lg">
+      <h2 className="text-2xl font-semibold mb-4">{event.title}</h2>
+      <p className="text-gray-700 mb-4">{event.description}</p>
+      <p className="text-gray-600">Organizirao/la: {event.organizer}</p>
     </div>
   );
 }
--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,59 +1,83 @@
 const events = [
   {
     id: 1,
-    title: "Game Night 1",
-    description: "Detalji o Game Night 1",
+    title: "Igraonica 1",
+    description:
+      "Igraonica u kojoj ćemo se zabavljati uz razne društvene igre.",
     organizer: "Pero",
+    date: new Date(2024, 0, 30),
+    city: "Zagreb",
   },
   {
     id: 2,
-    title: "Game Night 2",
-    description: "Detalji o Game Night 2",
+    title: "Igraonica 2",
+    description:
+      "Drugi po redu događaj igraonice. Pridružite nam se na večeri punoj igara.",
     organizer: "Bob",
+    date: new Date(2024, 1, 15),
+    city: "Split",
   },
   {
     id: 3,
-    title: "Game Night 3",
-    description: "Detalji o Game Night 2",
+    title: "Igraonica 3",
+    description:
+      "Treći događaj igraonice s mnoštvom novih igara za sve uzraste.",
     organizer: "Đuro",
+    date: new Date(2024, 2, 5),
+    city: "Rijeka",
   },
   {
     id: 4,
-    title: "Game Night 4",
-    description: "Detalji o Game Night 2",
+    title: "Board Game Night",
+    description: "Opustite se uz društvene igre na ovom događaju.",
     organizer: "Bob4",
+    date: new Date(2024, 2, 20),
+    city: "Osijek",
   },
   {
     id: 5,
-    title: "Game Night 5",
-    description: "Detalji o Game Night 2",
+    title: "Game Night XL",
+    description: "Velika noć igara za sve ljubitelje zabave.",
     organizer: "Bob5",
+    date: new Date(2024, 3, 10),
+    city: "Zadar",
   },
   {
     id: 6,
-    title: "Game Night 6",
-    description: "Detalji o Game Night 2",
+    title: "Retro Game Night",
+    description: "Povratak u prošlost s retro igrama i dozom nostalgije.",
     organizer: "Bob6",
+    date: new Date(2024, 4, 2),
+    city: "Pula",
   },
   {
     id: 7,
-    title: "Game Night 7",
-    description: "Detalji o Game Night 2",
+    title: "Family Game Night",
+    description: "Obiteljski događaj s igrama koje će zabaviti sve generacije.",
     organizer: "Bob7",
+    date: new Date(2024, 5, 15),
+    city: "Dubrovnik",
   },
   {
     id: 8,
-    title: "Game Night 8",
-    description: "Detalji o Game Night 2",
+    title: "Game Night Deluxe",
+    description: "Ekskluzivna noć igara s raznovrsnim izborom.",
     organizer: "Bob8",
+    date: new Date(2024, 6, 7),
+    city: "Šibenik",
   },
   {
     id: 9,
     title: "Game Night 9",
-    description: "Detalji o Game Night 2",
+    description:
+      "Deveti po redu događaj igraonice. Pridružite nam se i budite dio zabave.",
     organizer: "Bob9",
+    date: new Date(2024, 7, 1),
+    city: "Varaždin",
   },
 ];
+
+export default events;
 
 export function getEvents() {
   return events;
